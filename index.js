d2a1ed1995bdfd1fbb8313e2887381cc23c33aff
longbooks <114227398+longbooks@users.noreply.github.com>
2022-10-03 15:12:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
-    $("image").attr('src',profile.getImageUrl());
+    $("#image").attr('src',profile.getImageUrl());
     $(".data").css("display","block");
     $(".g-signin2").css("display","none");
+
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present
 }
 
 function signOut() {
