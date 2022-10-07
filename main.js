d32a20f12c4b789d383eaeb25f9ec30a235973af
longbooks <114227398+longbooks@users.noreply.github.com>
2022-10-07 13:07:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -49,13 +49,13 @@ function initClient() {
 // Update UI sign in state changes
 function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
-//     authorizeButton.style.display = 'none';
+    authorizeButton.style.display = 'none';
     signoutButton.style.display = 'block';
     content.style.display = 'block';
     videoContainer.style.display = 'block';
     getChannel(defaultChannel);
   } else {
-//     authorizeButton.style.display = 'block';
+    authorizeButton.style.display = 'block';
     signoutButton.style.display = 'none';
     content.style.display = 'none';
     videoContainer.style.display = 'none';
