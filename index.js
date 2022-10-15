c1dfddaf740143c6ab08da4af09d2b77667c66de
longbooks <114227398+longbooks@users.noreply.github.com>
2022-10-15 14:27:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+var client;
+var access_token;
+
+function initClient() {
+  client = google.accounts.oauth2.initTokenClient({
+    client_id: '141290470186-e63pmerr4gft8cep5gptbbjlo3gcqn5u.apps.googleusercontent.com',
+    scope: 'https://www.googleapis.com/auth/youtube.readonly',
+    callback: (tokenResponse) => {
+      if(tokenResponse && tokenResponse.access_token){
+        if(google.accounts.oauth2.hasGrantedAnyScope(tokenResponse,
+          'https://www.googleapis.com/auth/youtube.readonly')){
+            access_token = tokenResponse.access_token;
+        }else{
+          alert("alredy connected");
+        }
+      }
+    },
+  });
+}
+
+function getToken() {
+  client.requestAccessToken()
+}
+function revokeToken() {
+  google.accounts.oauth2.revoke(access_token, () => {console.log('access token revoked')});
+}
+
+function showChanneldata(data){
+  const channeldata=document.getElementById('channel-data');
+  channeldata.innerHTML=data;
+}
+function loadCalendar() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://youtube.googleapis.com/youtube/v3/channels?part=snippet,contentDetails,statistics&mine=true&key=AIzaSyBTJnaY-IbClJu84zEEv1lGYQlXHvQeLu0',true);
+  xhr.responseType="json";
+  xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+  xhr.onload=()=>{
+    if(xhr.status===200){
+      console.log(xhr.response);
+      const channel=xhr.response.items[0];
+      const output=`
+      <img src="${channel.snippet.thumbnails.medium.url}" height="${channel.snippet.thumbnails.medium.height}" width="${channel.snippet.thumbnails.medium.width}">
+      <ul class='collection'>
+        <li class='collection-item'>Id:${channel.id}</li>
+        <li class='collection-item'>Title:${channel.snippet.title}</li>
+        <li class='collection-item'>Subscribers:${channel.statistics.subscriberCount}</li>
+        <li class='collection-item'>Views:${channel.statistics.viewCount}</li>
+        <li class='collection-item'>Video:${channel.statistics.videoCount}</li>
+      </ul>
+      <hr>
+      <a class='btn' target="_blank" href="https://youtube.com/channel/${channel.id}">Visit Channel</a>
+      `;
+      showChanneldata(output);
+    }else{
+      console.log("Error");
+    }
+  };
+  xhr.send();
+}
