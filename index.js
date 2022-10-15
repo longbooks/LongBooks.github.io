da5340065db49828adf472392a07afd53b2f46f4
longbooks <114227398+longbooks@users.noreply.github.com>
2022-10-15 16:54:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,17 +20,27 @@ function revokeToken() {
   google.accounts.oauth2.revoke(access_token, () => {console.log('access token revoked')});
 }
 
+function showChanneldata(data){
+  const channeldata=document.getElementById('channel-data');
+  setInterval(function(){
+    channeldata.innerHTML=data;
+  });
+};
+
 document.addEventListener('DOMContentLoaded',function(){
-  function showChanneldata(data){
-    const channeldata=document.getElementById('channel-data');
-    setInterval(function(){
-      channeldata.innerHTML=data;
-    });
-  };
+  showChanneldata();
 })
 
-function channeldata(channel){
-  const output=`
+function loadCalendar() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://youtube.googleapis.com/youtube/v3/channels?part=snippet,contentDetails,statistics&mine=true&key=AIzaSyBTJnaY-IbClJu84zEEv1lGYQlXHvQeLu0',true);
+  xhr.responseType="json";
+  xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+  xhr.onload=()=>{
+    if(xhr.status===200){
+      console.log(xhr.response);
+      const channel=xhr.response.items[0];
+      const output=`
       <img src="${channel.snippet.thumbnails.medium.url}" height="${channel.snippet.thumbnails.medium.height}" width="${channel.snippet.thumbnails.medium.width}">
       <ul class='collection'>
         <li class='collection-item'>Id:${channel.id}</li>
@@ -43,18 +53,6 @@ function channeldata(channel){
       <a class='btn' target="_blank" href="https://youtube.com/channel/${channel.id}">Visit Channel</a>
       `;
       showChanneldata(output);
-}
-
-function loadCalendar() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://youtube.googleapis.com/youtube/v3/channels?part=snippet,contentDetails,statistics&mine=true&key=AIzaSyBTJnaY-IbClJu84zEEv1lGYQlXHvQeLu0',true);
-  xhr.responseType="json";
-  xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-  xhr.onload=()=>{
-    if(xhr.status===200){
-      console.log(xhr.response);
-      const channel=xhr.response.items[0];
-      channeldata(channel);
     }else{
       console.log("Error");
     }
